b862558e02d7b7dbc00f6f8bb7b3a9eaa802bb17
thince-mscs <70350120+thince-mscs@users.noreply.github.com>
2020-09-02 23:46:21 -0500

    Updated Assignment 1c1

---
+++ thince1c1/frmFoodTruck.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace thince1c1
+{
+    public partial class frmFoodTruck : Form
+    {
+        public frmFoodTruck()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            txtHotDogsSubtotal.Text = (
+                    4.0m * Convert.ToDecimal(txtHotDogs.Text)
+                ).ToString("0.00");
+            txtHamburgersSubtotal.Text = (
+                   5.0m * Convert.ToDecimal(txtHamburgers.Text)
+               ).ToString("0.00");
+            txtPretaxTotal.Text = (
+                Convert.ToDecimal(txtHotDogsSubtotal.Text) + Convert.ToDecimal(txtHamburgersSubtotal.Text)
+             ).ToString("0.00");
+            txtTax.Text =
+            (Convert.ToDecimal(txtPretaxTotal.Text)
+            * Convert.ToDecimal(0.06875)).ToString("0.00");
+            txtTotal.Text = (Convert.ToDecimal(txtPretaxTotal.Text) + Convert.ToDecimal(txtTax.Text)).ToString("0.00");
+        }
+
+        private void frmFoodTruck_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtHamburgersSubtotal_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
