3f2dd410b7a74d711ea68c7171daa1edb29beed7
thince-mscs <70350120+thince-mscs@users.noreply.github.com>
2020-10-27 14:19:51 -0500

    Updated after video #35

---
+++ thince2f1/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace thince2f1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.input1ATextbox = new System.Windows.Forms.TextBox();
+            this.result6TextBox = new System.Windows.Forms.TextBox();
+            this.result5TextBox = new System.Windows.Forms.TextBox();
+            this.result4TextBox = new System.Windows.Forms.TextBox();
+            this.result2TextBox = new System.Windows.Forms.TextBox();
+            this.result3TextBox = new System.Windows.Forms.TextBox();
+            this.result1TextBox = new System.Windows.Forms.TextBox();
+            this.input6BTextbox = new System.Windows.Forms.TextBox();
+            this.input2ATextbox = new System.Windows.Forms.TextBox();
+            this.input3ATextbox = new System.Windows.Forms.TextBox();
+            this.input4ATextbox = new System.Windows.Forms.TextBox();
+            this.input5ATextbox = new System.Windows.Forms.TextBox();
+            this.input6ATextbox = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1) if";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "2) if {block}";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "3) if else";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "4) if else if";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 159);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "5) Better range test";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(31, 185);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "6) Nested if else";
+            // 
+            // input1ATextbox
+            // 
+            this.input1ATextbox.Location = new System.Drawing.Point(159, 52);
+            this.input1ATextbox.Name = "input1ATextbox";
+            this.input1ATextbox.Size = new System.Drawing.Size(100, 20);
+            this.input1ATextbox.TabIndex = 6;
+            this.input1ATextbox.Text = "100.00";
+            this.input1ATextbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // result6TextBox
+            // 
+            this.result6TextBox.Location = new System.Drawing.Point(313, 182);
+            this.result6TextBox.Name = "result6TextBox";
+            this.result6TextBox.ReadOnly = true;
+            this.result6TextBox.Size = new System.Drawing.Size(120, 20);
+            this.result6TextBox.TabIndex = 17;
+            // 
+            // result5TextBox
+            // 
+            this.result5TextBox.Location = new System.Drawing.Point(313, 156);
+            this.result5TextBox.Name = "result5TextBox";
+            this.result5TextBox.ReadOnly = true;
+            this.result5TextBox.Size = new System.Drawing.Size(120, 20);
+            this.result5TextBox.TabIndex = 16;
+            // 
+            // result4TextBox
+            // 
+            this.result4TextBox.Location = new System.Drawing.Point(313, 130);
+            this.result4TextBox.Name = "result4TextBox";
+            this.result4TextBox.ReadOnly = true;
+            this.result4TextBox.Size = new System.Drawing.Size(120, 20);
+            this.result4TextBox.TabIndex = 15;
+            // 
+            // result2TextBox
+            // 
+            this.result2TextBox.Location = new System.Drawing.Point(313, 78);
+            this.result2TextBox.Name = "result2TextBox";
+            this.result2TextBox.ReadOnly = true;
+            this.result2TextBox.Size = new System.Drawing.Size(120, 20);
+            this.result2TextBox.TabIndex = 14;
+            // 
+            // result3TextBox
+            // 
+            this.result3TextBox.Location = new System.Drawing.Point(313, 104);
+            this.result3TextBox.Name = "result3TextBox";
+            this.result3TextBox.ReadOnly = true;
+            this.result3TextBox.Size = new System.Drawing.Size(120, 20);
+            this.result3TextBox.TabIndex = 13;
+            // 
+            // result1TextBox
+            // 
+            this.result1TextBox.Location = new System.Drawing.Point(313, 52);
+            this.result1TextBox.Name = "result1TextBox";
+            this.result1TextBox.ReadOnly = true;
+            this.result1TextBox.Size = new System.Drawing.Size(120, 20);
+            this.result1TextBox.TabIndex = 12;
+            // 
+            // input6BTextbox
+            // 
+            this.input6BTextbox.Location = new System.Drawing.Point(265, 182);
+            this.input6BTextbox.Name = "input6BTextbox";
+            this.input6BTextbox.Size = new System.Drawing.Size(40, 20);
+            this.input6BTextbox.TabIndex = 18;
+            // 
+            // input2ATextbox
+            // 
+            this.input2ATextbox.Location = new System.Drawing.Point(159, 78);
+            this.input2ATextbox.Name = "input2ATextbox";
+            this.input2ATextbox.Size = new System.Drawing.Size(100, 20);
+            this.input2ATextbox.TabIndex = 19;
+            this.input2ATextbox.Text = "100.00";
+            this.input2ATextbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // input3ATextbox
+            // 
+            this.input3ATextbox.Location = new System.Drawing.Point(159, 104);
+            this.input3ATextbox.Name = "input3ATextbox";
+            this.input3ATextbox.Size = new System.Drawing.Size(100, 20);
+            this.input3ATextbox.TabIndex = 20;
+            this.input3ATextbox.Text = "100.00";
+            this.input3ATextbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // input4ATextbox
+            // 
+            this.input4ATextbox.Location = new System.Drawing.Point(159, 130);
+            this.input4ATextbox.Name = "input4ATextbox";
+            this.input4ATextbox.Size = new System.Drawing.Size(100, 20);
+            this.input4ATextbox.TabIndex = 21;
+            this.input4ATextbox.Text = "100.00";
+            this.input4ATextbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // input5ATextbox
+            // 
+            this.input5ATextbox.Location = new System.Drawing.Point(159, 156);
+            this.input5ATextbox.Name = "input5ATextbox";
+            this.input5ATextbox.Size = new System.Drawing.Size(100, 20);
+            this.input5ATextbox.TabIndex = 22;
+            this.input5ATextbox.Text = "100.00";
+            this.input5ATextbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // input6ATextbox
+            // 
+            this.input6ATextbox.Location = new System.Drawing.Point(159, 182);
+            this.input6ATextbox.Name = "input6ATextbox";
+            this.input6ATextbox.Size = new System.Drawing.Size(100, 20);
+            this.input6ATextbox.TabIndex = 23;
+            this.input6ATextbox.Text = "100.00";
+            this.input6ATextbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(230, 208);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 24;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(552, 329);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.input6ATextbox);
+            this.Controls.Add(this.input5ATextbox);
+            this.Controls.Add(this.input4ATextbox);
+            this.Controls.Add(this.input3ATextbox);
+            this.Controls.Add(this.input2ATextbox);
+            this.Controls.Add(this.input6BTextbox);
+            this.Controls.Add(this.result6TextBox);
+            this.Controls.Add(this.result5TextBox);
+            this.Controls.Add(this.result4TextBox);
+            this.Controls.Add(this.result2TextBox);
+            this.Controls.Add(this.result3TextBox);
+            this.Controls.Add(this.result1TextBox);
+            this.Controls.Add(this.input1ATextbox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox input1ATextbox;
+        private System.Windows.Forms.TextBox result6TextBox;
+        private System.Windows.Forms.TextBox result5TextBox;
+        private System.Windows.Forms.TextBox result4TextBox;
+        private System.Windows.Forms.TextBox result2TextBox;
+        private System.Windows.Forms.TextBox result3TextBox;
+        private System.Windows.Forms.TextBox result1TextBox;
+        private System.Windows.Forms.TextBox input6BTextbox;
+        private System.Windows.Forms.TextBox input2ATextbox;
+        private System.Windows.Forms.TextBox input3ATextbox;
+        private System.Windows.Forms.TextBox input4ATextbox;
+        private System.Windows.Forms.TextBox input5ATextbox;
+        private System.Windows.Forms.TextBox input6ATextbox;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ thince2f1/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace thince2f1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            decimal subtotal = 0m;
+            decimal discountPercent = 0m;
+
+            // #1 if
+            subtotal = Decimal.Parse(input1ATextbox.Text);
+            if (subtotal >= 100m)
+               discountPercent = 0.2m;
+            result1TextBox.Text = discountPercent.ToString("n2");
+
+            // #2: if {block}         
+            subtotal = Decimal.Parse(input2ATextbox.Text); 
+            discountPercent = 0m;
+            string status = "Standard rate: ";
+            if (subtotal >= 100m)
+            {
+                discountPercent = 0.2m;
+                status = "Bulk rate: ";
+            }
+            result2TextBox.Text = status + discountPercent.ToString("n2");
+
+
+            // #3: if else          
+            subtotal = Decimal.Parse(input3ATextbox.Text);
+            discountPercent = 0m;
+            if (subtotal >= 100m)
+                discountPercent = 0.2m;
+            else
+                discountPercent = 0.1m;
+            result3TextBox.Text = discountPercent.ToString("n2");
+
+            // #4:
+            subtotal = Decimal.Parse(input4ATextbox.Text);
+            discountPercent = 0m;
+            if (subtotal >= 100 && subtotal < 200)
+                discountPercent = 0.2m;
+            else if (subtotal >= 200m && subtotal < 300)
+                discountPercent = 0.3m;
+            else if (subtotal >= 300m)
+                discountPercent = 0.4m;
+            else
+                discountPercent = 0.1m;
+            result4TextBox.Text = discountPercent.ToString("n2");
+
+            // #5: Better range test
+            subtotal = Decimal.Parse(input5ATextbox.Text);
+            discountPercent = 0m;
+            if (subtotal >= 300m)
+                discountPercent = 0.4m;
+            else if (subtotal >=  200m)
+                discountPercent = 0.3m;
+            else if (subtotal >= 100m)
+                discountPercent = 0.2m;
+            else
+                discountPercent = 0.1m;
+            result5TextBox.Text = discountPercent.ToString("n2");
+
+            // #6:
+            subtotal = Decimal.Parse(input6BTextbox.Text);
+            string customerType = input6BTextbox.Text;
+            discountPercent = 0m;
+            if (customerType == "R")
+            {
+                if (subtotal >= 100m)
+                    discountPercent = 0.2m;
+                else
+                    discountPercent = 0.1m;
+            }
+            else            // customerType isn't "R"
+                discountPercent = 0.4m;
+            result6TextBox.Text = discountPercent.ToString("n2");
+
+        }
+    }
+}
