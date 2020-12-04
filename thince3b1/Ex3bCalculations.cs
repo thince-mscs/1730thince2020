aae7ea7d286adbd8f69f56d53ee18feff9103914
thince-mscs <70350120+thince-mscs@users.noreply.github.com>
2020-12-04 11:20:11 -0600

    Updated with issues

---
+++ thince3b1/Ex3bCalculations.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thince3b1
+{
+    public class Ex3bCalculations
+    {
+        public static decimal GetDiscountPercent(decimal subtotal)
+        {
+            decimal discountPercent = 0m;
+
+            if (subtotal >= 500m)
+                discountPercent = 0.2m;
+            else
+                discountPercent = 0.1m;
+
+
+            return discountPercent;
+        }
+        public static decimal CalculateFutureValue(decimal monthlyInvestment, decimal monthlyInterestRate, int months) // FOR loop
+        {
+            decimal futureValue = 0m;
+
+            for (int i = 0; i < months; i++)
+            {
+                futureValue = (futureValue + monthlyInvestment) * (1 + monthlyInterestRate);
+            }
+
+            return futureValue;
+        }
+        public static double FahrenheitToCelsius(double Fahrenheit, double Celsius);
+        {         
+            double celsius = 36;
+
+                fahrenheit = (celsius - 32) * 5 / 9;
+        
+
+
+            return celsius;
+
+
+    }
+}
---
+++ thince3b1/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace thince3b1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calcButton_Click(object sender, RoutedEventArgs e) 
+            // Method GetDiscountPercent pass it as a decimal (subtotal) 
+            // return decimal as (discountPercent)
+            // store it here in DP then display result as discountPercent.Tostring("f3");
+        {
+            try
+            {
+                decimal subtotal = Decimal.Parse(inputTextBox0a.Text);
+                decimal discountPercent = Ex3bCalculations.GetDiscountPercent(subtotal);
+                resultTextBox0.Text = discountPercent.ToString("f3");
+            }
+            catch
+            {
+                MessageBox.Show("Invalid input: " + this.inputTextBox0a.Text);
+            }
+            try
+            {
+                int months = Int32.Parse(this.inputTextBox2a.Text);                    // go to textbox2a parse it as an int and put it into months
+                decimal monthlyInvestment = Decimal.Parse(this.inputTextBox2b.Text);   // go to textbox2b parse it and put it into monthlyInvestments
+                decimal monthlyInterestRate = Decimal.Parse(this.inputTextBox2c.Text); // go to textbox2c parse it as a decimal and copy it into monthlyInterestRate
+                decimal futureValue = Ex3bCalculations.CalculateFutureValue(monthlyInvestment, monthlyInterestRate, months);
+                resultTextBox2.Text = futureValue.ToString("c2");
+            }
+            catch
+            {
+                MessageBox.Show("Invalid input:\n"
+                    + this.inputTextBox2a.Text + "\n"
+                    + this.inputTextBox2b.Text + "\n"
+                    + this.inputTextBox2c.Text + "\n");
+            }
+        }
+    }
+}
